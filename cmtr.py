8db1fe039b6dbd5fd6da8b985e8e22aaa3bb82d7
kito0 <drewshah0@gmail.com>
2021-12-20 19:28:41 -0700

    Add repo init, default config, and find

--- cmtr.py
+++ cmtr.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
-import libcommitr
+import libcmtr
 
-libcommitr.main()
+libcmtr.main()
--- libcmtr.py
+++ libcmtr.py
@@ -43,3 +43,124 @@ def main(argv=sys.argv[1:]):
         cmd_show_ref(args)
     elif args.command == "tag":
         cmd_tag(args)
+
+
+class GitRespository(object):
+    worktree = None
+    gitdir = None
+    conf = None
+
+    def __init__(self, path, force=False):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception("Not a Git repository %s" % path)
+
+            self.conf = configparser.ConfigParser()
+            cf = repo_file(self, "config")
+
+            if cf and os.path.exists(cf):
+                self.conf.read([cf])
+            elif not force:
+                raise Exception("Configuration file missing")
+
+            if not force:
+                vers = int(self.conf.get("core", "repositoryformatversion"))
+                if vers != 0:
+                    raise Exception(
+                        "Unsupported repositoryformatversion %s" % vers)
+
+
+def repo_path(repo, *path):
+    return os.path.join(repo.gitdir, *path)
+
+
+def repo_file(repo, *path, mkdir=False):
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+
+
+def repo_dir(repo, *path, mkdir=False):
+    path = repo_path(repo, *path)
+
+    if os.path.exists(path):
+        if os.path.isdir(path):
+            return path
+        else:
+            raise Exception("Not a directory %s" % path)
+
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
+
+
+def repo_create(path):
+    repo = GitRespository(path, True)
+
+    if os.path.exists(repo.worktree):
+        if not os.path.isdir(repo.worktree):
+            raise Exception("%s is not a directory!" % path)
+        if os.listdir(repo.worktree):
+            raise Exception("%s is not empty!" % path)
+    else:
+        os.makedirs(repo.worktree)
+
+    assert(repo_dir(repo, "branches", mkdir=True))
+    assert(repo_dir(repo, "objects", mkdir=True))
+    assert(repo_dir(repo, "refs", "tags", mkdir=True))
+    assert(repo_dir(repo, "refs", "heads", mkdir=True))
+
+    with open(repo_file(repo, "description"), "w") as f:
+        f.write(
+            "Unnamed repository: edit this file 'description' to name the repository.\n")
+
+    with open(repo_file(repo, "HEAD"), "w") as f:
+        f.write("ref: refs/head/master\n")
+
+    with open(repo_file(repo, "config"), "w") as f:
+        config = repo_default_config()
+        config.write(f)
+
+    return repo
+
+
+def repo_default_config():
+    ret = configparser.ConfigParser()
+
+    ret.add_section("core")
+    ret.set("core", "repositoryformatversion", "0")
+    ret.set("core", "filemode", "false")
+    ret.set("core", "bare", "false")
+
+    return ret
+
+
+argsp = argsubparsers.add_parser(
+    "init", help="initialize a new, empty repository.")
+
+argsp.add_argumennt("path", metavar="directory", nargs="?",
+                    default=".", help="Where to create the repository.")
+
+
+def cmd_init(args):
+    repo_create(args.path)
+
+
+def repo_find(path=".", required=True):
+    path = os.path.realpath(path)
+
+    if os.path.isdir(os.path.join(path, ".git")):
+        return GitRespository(path)
+
+    parent = os.path.reapath(os.path.join(path, ".."))
+
+    if parent == path:
+        if required:
+            raise Exception("No git directory.")
+        else:
+            return None
+
+    return repo_find(parent, required)
